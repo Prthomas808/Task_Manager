dea6bc8c6df6c4a8fee691d93337fc5db3b46f09
Pedro <prthomas808@gmail.com>
2024-01-06 23:41:46 -0500

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Pedro Thomas on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
